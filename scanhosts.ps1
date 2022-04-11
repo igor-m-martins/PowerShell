b6d6db305cb2ed199db16af9206bab7908a2b667
Igor M. Martins <52651128+igor-m-martins@users.noreply.github.com>
2022-04-11 18:16:53 +0000

    Create scanhosts.ps1

---
+++ scanhosts.ps1
@@ -0,0 +1,13 @@
+echo "Desec Security - Descoberta de Hosts"
+param($p1)
+
+if (!$p1){
+    echo "Exemplo de uso: .\scanhosts.ps1 192.168.0"
+} else {
+foreach ($ip in 1..254){
+        try{
+        $resp = ping -n 1 "$p1.$ip" | Select-String "bytes=32"
+        $resp.Line.split(' ')[2] -replace ":",""
+        } catch {}
+    }
+}
